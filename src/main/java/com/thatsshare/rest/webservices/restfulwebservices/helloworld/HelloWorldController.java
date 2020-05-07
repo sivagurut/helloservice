081979f84aadcb9b94977c6e385688b9539f5ce2
sivagurut <sivagurut@gmail.com>
2020-05-07 13:39:47 +0530

    Update HelloWorldController.java

--- src/main/java/com/thatsshare/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/thatsshare/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.thatsshare.rest.webservices.restfulwebservices.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloWorldController {
